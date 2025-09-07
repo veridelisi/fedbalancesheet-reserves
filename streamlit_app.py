45bef5167683123334894cf7468727a5c29c8f28
veridelisi <72710290+veridelisi@users.noreply.github.com>
2025-09-07 01:43:00 +0000

    Fix nav

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,3 @@ import streamlit as st
 
 st.page_link("streamlit_app.py", label="Home", icon="🏠")
 st.page_link("pages/01_Reserves.py", label="Page 1", icon="1️⃣")
-st.page_link("pages/page_2.py", label="Page 2", icon="2️⃣", disabled=True)
-st.page_link("http://www.google.com", label="Google", icon="🌎")
