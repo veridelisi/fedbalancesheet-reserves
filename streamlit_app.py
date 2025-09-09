aaffa38053a04bba88715a8c96046c5adbb99d36
veridelisi <72710290+veridelisi@users.noreply.github.com>
2025-09-09 19:25:22 +0000

    Fix nav

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,13 +9,7 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+
 
 
 st.markdown(
